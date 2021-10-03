b58f7715fe18241d9675603e2424804f1217bca2
Ankitkundu21 <91826508+Ankitkundu21@users.noreply.github.com>
2021-10-03 23:02:24 +0530

    Create python-code-bunch.py

---
+++ python-code-bunch.py
@@ -0,0 +1,13 @@
+#SUM OF DIGITS [USING append()]
+
+#SOLUTION :
+digits=[]
+sum=0
+for i in range(21):
+     digits.append(i)
+for i in digits:
+     sum+=i       
+print(digits)
+print("Sum of digits is : ", sum)
+
+            
