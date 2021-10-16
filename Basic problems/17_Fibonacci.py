70b23aa05735cabbecc24b3c99688ec2318aa30e
hv4114 <92599851+hv4114@users.noreply.github.com>
2021-10-16 16:11:25 +0530

    Easier_way_fibonacci.py
    I found a less complex way to perform the same task.

--- Basic problems/17_Fibonacci.py
+++ Basic problems/17_Fibonacci.py
@@ -1,15 +1,8 @@
 n=int(input("Enter number of terms\n"))
 a=0
 b=1
-c=0
-if n<=0:
-    print("Enter only positive numbers")
-elif(n==1):
-    print(a)
-else:
-    while(c<n):
-        print(a,end=" ")
-        x=a+b
-        a=b
-        b=x
-        c=c+1
+print(a, b,end=' ')
+for j in range(1,n-1):
+c = a+b
+print(c)
+a,b=b,c
