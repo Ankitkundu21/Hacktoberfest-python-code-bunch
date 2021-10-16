718d49091b575c903e7fc64128a076648a6e4c5e
xVanshx <84569056+xVanshx@users.noreply.github.com>
2021-10-16 16:43:17 +0530

    Create basicCoffeeOrderBot.py

---
+++ basicCoffeeOrderBot.py
@@ -0,0 +1,6 @@
+name = input("What is your name sir\Ma'am \n>")
+print("Hello " + name + " here's our menu.")
+menu = " Coffee\n Burger\n Pizza\n Noodles"
+print(menu)
+order = input(name + " would you like to order \n >")
+print("Thanks!! for ordering "+ name +" your order " + order + " will be ready soon!")
