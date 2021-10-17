d5a08f1fd8e520101d1920fcdc3ed00db5013fab
sodiptabadiabanurea <33195451+sodiptabadiabanurea@users.noreply.github.com>
2021-10-17 22:13:54 +0900

    Update 20_prime.py
    Change 2 to 3 in line 5

--- Basic problems/20_prime.py
+++ Basic problems/20_prime.py
@@ -2,7 +2,7 @@ a=int(input())
 b=int(input())
 for n in range(a,b+1):
     if n>1:
-        for i in range(2,n):
+        for i in range(3,n):
             if(n%i==0):
                 break
         else:
